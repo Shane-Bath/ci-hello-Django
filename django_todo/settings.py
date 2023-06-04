acb57559632e9db8c1113ee1e4fa2736470647cc
Shane-Bath <>
2023-06-04 09:48:26 +0000

    Fixed hostname issue

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECERT_KEY', 'django-insecure-_+7qi&n=3wpr+a7p=i4r^aeom8t#xcmvc6hzd8wy)syg7_8rr#')
+SECRET_KEY = os.environ.get(
+    'SECERT_KEY', 'django-insecure-_+7qi&n=3wpr+a7p=i4r^aeom8t#xcmvc6hzd8wy)syg7_8rr#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HEROHU_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
