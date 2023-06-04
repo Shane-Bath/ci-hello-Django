c6311e80529329f3e1144d512170f91fc101df9b
Shane-Bath <>
2023-06-04 09:29:06 +0000

    Changed DATABASE URL

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 #     }
 # }
 
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-# }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
-os.environ.get("DATABASE_URL")
+# os.environ.get("DATABASE_URL")
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
